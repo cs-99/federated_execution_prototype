50bba2f806c85889766e1d89ec4e7934eedb122a
unknown <ckhuemonma@web.de>
2023-11-21 00:56:30 +0100

    implemented actual protocol

--- reactor.py
+++ reactor.py
@@ -3,7 +3,9 @@ This is a prototype for the decentralized coordination contract used for federat
 """
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Callable, List, Optional, Dict, Tuple
+from msilib.schema import RadioButton
+from multiprocessing.connection import Connection
+from typing import Callable, List, Optional, Tuple
 import threading
 # custom files from here
 from tag import Tag
@@ -24,10 +26,6 @@ class RegisteredReactor:
     def __init__(self, reactor : Reactor) -> None:
         assert isinstance(reactor, Reactor)
         self._reactor : Reactor = reactor
-
-    @property
-    def reactor(self) -> Reactor:
-        return self._reactor
 
 class Action(Named, RegisteredReactor):
     def __init__(self, name, reactor : Reactor):
@@ -60,14 +58,20 @@ class Timer(TriggerAction):
         assert isinstance(interval, int)
         self._offset : int = offset
         self._interval : int = interval
-        #self._set_func(lambda tag: self._reactor.schedule_action_async(self, Tag(tag.time + self._interval)))
+        self._set_func(self._on_exec)
 
     def __repr__(self) -> str:
         return f"{super().__repr__()}({utility.ns_to_secs(self._offset)}, {utility.ns_to_secs(self._interval)})"
 
     def schedule_start(self):
-        self._reactor.schedule_action_sync(self, Tag(self._reactor.start_tag.time + self._offset))
-        
+        first_tag = Tag(self._reactor.start_tag.time + self._offset)
+        if first_tag < self._reactor.stop_tag:
+            self._reactor.schedule_action_sync(self, first_tag)
+
+    def _on_exec(self, tag: Tag):
+        next_tag = Tag(tag.time + self._interval)
+        if next_tag < self._reactor.stop_tag:
+            self._reactor.schedule_action_async(self, next_tag)
         
 
 class Port(Named, RegisteredReactor):
@@ -82,72 +86,107 @@ class Input(Port, TriggerAction):
     def __init__(self, name, reactor  : Reactor):
         Port.__init__(self, name, reactor)
         TriggerAction.__init__(self, name, reactor)
+        self._released_tag : Tag = Tag(0)
+        self._is_connected : bool = False
+
+    def set_connected(self):
+        self._is_connected = True
+
+    def _update_released_tag(self, tag : Tag):
+        if tag > self._released_tag:
+            self._released_tag = tag
+    
+    def receive_message(self, tag : Tag):
+        self._update_released_tag(tag)
+    
+    def receive_tag_only(self, tag : Tag):
+        self._update_released_tag(tag)
+
+    def acquire_tag(self, tag : Tag, predicate : Callable[[], bool] = lambda: False):
+        if tag <= self._released_tag or not self._is_connected:
+            return True
+        CommunicationBus().send_tag_request(self, tag) # schedule empty event at sender
+        self._reactor.wait_for(lambda: tag <= self._released_tag or predicate)
 
 class Output(Port):
     def __init__(self, name, reactor : Reactor):
         super().__init__(name, reactor)
-        reactor.register_release_tag_callback(lambda tag: CommunicationBus().set_tag_only(self, tag))
+        reactor.register_release_tag_callback(lambda tag: CommunicationBus().send_tag_only(self, tag))
 
+    def set(self, tag : Tag):
+        CommunicationBus().send(self, tag)
+
+    def process_tag_request(self, tag : Tag):
+        if not self._reactor.schedule_empty_async_at(tag): # scheduling failed -> we are at a later tag already
+            CommunicationBus().send_tag_only(self, self._reactor.current_tag)
+    
+# represents a connection from one output to n inputs
+# calls the relevant methods on Inputs and Output
 class Connection:
-        @property 
-        def input(self) -> Input:
-            return self._input
-        
-        @property
-        def output(self) -> Output:
-            return self._output
+    @property 
+    def inputs(self) -> List[Input]:
+        return self._inputs
+    
+    @property
+    def output(self) -> Output:
+        return self._output
 
-        @property
-        def is_set(self) -> bool: 
-            return self._is_set
+    def __init__(self, output : Output, inputs : List[Input]) -> None:
+        if not isinstance(output, Output):
+            raise ValueError("Connections must have an output.")
+        assert len(inputs) > 0
+        utility.list_instance_check(inputs, Input)
+        self._inputs : List[Input] = inputs
+        self._output : Output = output
 
-        @property
-        def tag(self) -> Tag:
-            return self._tag
+    def send_message(self, tag : Tag) -> None:
+        for input in self._inputs:
+            input.receive_message(tag)
 
-        def __init__(self, output, input) -> None:
-            if not isinstance(output, Output) or not isinstance(input, Input):
-                raise ValueError("Connections must consist of output and input.")
-            self._input : Input = input
-            self._output : Output = output
-            self._is_set : bool = False
-            self._tag : Optional[Tag] = None
+    def send_tag_only(self, tag : Tag) -> None:
+        for input in self._inputs:
+            input.receive_tag_only(tag)
 
-        def set_message(self, tag) -> None:
-            self._is_set = True
-            self._tag = tag
-            # todo schedule on input
+    def send_tag_request(self, tag : Tag) -> None:
+        self._output.process_tag_request(tag)
 
-        def set_tag_only(self, tag) -> None:
-            self._tag = tag
-            # todo schedule on input
-
-        def unset_message(self) -> None:
-            self._is_set = False
-
+ 
+# singleton that manages all connections
 class CommunicationBus(metaclass=utility.Singleton):
     def __init__(self):
         self._connections : List[Connection] = []
         self._connection_lock : threading.Lock = threading.Lock()
 
-    def add_connection(self, output, input) -> None:
-        self._connection_lock.acquire()
-        self._connections.append(Connection(output, input)) 
-        self._connection_lock.release()
+    def _check_input_already_connected(self, input : Input):
+        for con in self._connections:
+            if input in con.inputs:
+                raise ValueError(f"Input {input.name} already connected to {con.output}")
 
-    def set(self, output: Output, tag : Tag) -> None:
-        self._connection_lock.acquire()
-        for connection in self._connections:
-            if connection.output == output:
-                connection.set_message(tag)
-        self._connection_lock.release()
+    def add_connection(self, output, inputs : List[Input]) -> None:
+        with self._connection_lock:
+            for input in inputs:
+                self._check_input_already_connected(input)
+                input.set_connected()
+            self._connections.append(Connection(output, inputs)) 
 
-    def set_tag_only(self, output: Output, tag : Tag) -> None:
-        self._connection_lock.acquire()
-        for connection in self._connections:
-            if connection.output == output:
-                connection.set_tag_only(tag)
-        self._connection_lock.release()
+    def send(self, output: Output, tag : Tag) -> None:
+        with self._connection_lock:
+            for connection in self._connections:
+                if connection.output == output:
+                    connection.send_message(tag)
+
+    def send_tag_only(self, output: Output, tag : Tag) -> None:
+        with self._connection_lock:
+            for connection in self._connections:
+                if connection.output == output:
+                    connection.send_tag_only(tag)
+
+    def send_tag_request(self, input : Input, tag : Tag) -> None:
+        with self._connection_lock:
+            for connection in self._connections:
+                if input in connection.inputs:
+                    connection.send_tag_request(tag)
+
 
 @dataclass
 class ReactionDeclaration:
@@ -163,6 +202,11 @@ class Reaction(Action):
         self._effects : List[Output] = effects if effects is not None else []
         utility.list_instance_check(self._triggers, TriggerAction)
         utility.list_instance_check(self._effects, Output)
+        self._set_func(self._set_outputs)
+
+    def _set_outputs(self, tag: Tag):
+        for effect in self._effects:
+            effect.set(tag)
 
     def __repr__(self) -> str:
         r = self.name
@@ -177,6 +221,7 @@ class Reaction(Action):
     @property
     def effects(self) -> List[Output]:
         return self._effects
+
 
 @dataclass
 class ActionListEntry:
@@ -194,15 +239,19 @@ class ActionList:
                 return
         self._list.append(ActionListEntry(tag, [action]))
 
-    def get_next(self) -> Tuple[Tag, List[Action]]:
+    def pop_tag(self, tag: Tag) -> List[Action]:
         assert len(self._list) > 0
-        smallest_tag_index : int = 0
-        smallest_tag : Tag = self._list[smallest_tag_index].tag
-        for index, entry in enumerate(self._list):
+        actions = list(filter(lambda e: e.tag == self.next_tag(), self._list))[0].actions
+        self._list = list(filter(lambda e: e.tag != self.next_tag(), self._list))
+        return actions
+    
+    def next_tag(self) -> Tag:
+        assert len(self._list) > 0
+        smallest_tag : Tag = self._list[0].tag
+        for entry in self._list:
             if entry.tag < smallest_tag:
                 smallest_tag = entry.tag 
-                smallest_tag_index = index
-        return self._list.pop(smallest_tag_index)
+        return smallest_tag
         
     def __bool__(self) -> bool:
         return bool(self._list)
@@ -241,12 +290,22 @@ class Reactor(Named):
         # scheduling 
         self._current_tag : Tag = self._start_tag.decrement()
         self._reaction_q : ActionList = ActionList()
+        self._reaction_q.add(self._start_tag, Action("__start__", self))
+        self._reaction_q.add(self._stop_tag, Action("__stop__", self))
         self._reaction_q_lock : threading.Lock = threading.Lock()
         self._reaction_q_cv : threading.Condition = threading.Condition(self._reaction_q_lock)
         
     @property
     def start_tag(self) -> Tag:
         return self._start_tag
+
+    @property 
+    def stop_tag(self) -> Tag:
+        return self._stop_tag
+
+    @property
+    def current_tag(self) -> Tag:
+        return self._current_tag
 
 
     def _init_reaction(self, reaction : ReactionDeclaration) -> Reaction:
@@ -271,6 +330,8 @@ class Reactor(Named):
 
     def __repr__(self) -> str:
         r = super().__repr__()
+        r += f"\n\t Start tag: {self.start_tag}"
+        r += f"\n\t Stop tag: {self.stop_tag}"
         r += f"\n\t Inputs: {', '.join(repr(input) for input in self._inputs)}"
         r += f"\n\t Outputs: {', '.join(repr(output) for output in self._outputs)}"
         r += f"\n\t Timers: {', '.join(repr(timer) for timer in self._timers)}"
@@ -296,7 +357,8 @@ class Reactor(Named):
         return self._inputs + self._outputs
 
     def _find_member_by_name(self, name) -> Optional[Named]:
-        for member in self._inputs + self._outputs + self._reactions + self._timers:
+        members : List[Named] = self._inputs + self._outputs + self._reactions + self._timers
+        for member in members:
             if member.name == name:
                 return member
         return None
@@ -312,9 +374,12 @@ class Reactor(Named):
         for callback in self._release_tag_callbacks:
             callback(self._current_tag)
 
+    def schedule_empty_async_at(self, tag : Tag) -> bool:
+        return self.schedule_action_async(Action("__empty__", self), tag)
+
     def schedule_action_async(self, action : Action, tag : Optional[Tag]=None) -> bool:
         with self._reaction_q_cv:
-            if tag <= self._current_tag:
+            if tag <= self._current_tag or tag >= self._stop_tag:
                 return False
             self.schedule_action_sync(action, tag)
             self._reaction_q_cv.notify()
@@ -323,6 +388,7 @@ class Reactor(Named):
     # this method assumes that self._reaction_q_cv is held
     def schedule_action_sync(self, action : Action, tag : Optional[Tag] = None) -> bool:
         assert tag > self._current_tag
+        assert tag < self._stop_tag
         assert self._reaction_q_lock.locked()
         if tag is None:
             tag = self._current_tag.delay()
@@ -332,45 +398,62 @@ class Reactor(Named):
     # unused, as the current tag gets initialized to the start_tag.decrement at construction time already
     # and therefore requests from other federates to schedule empty events will fail anyway when starting threads with .run()
     def _remove_tags_lte_current(self):
-        with self._reaction_q_cv:
-            tags_to_remove = []
-            for tag in self._reaction_q.keys():
-                if tag <= self._current_tag:
-                    tags_to_remove.append(tag)
-            for tag in tags_to_remove:
-                del self._reaction_q[tag]
+        tags_to_remove = []
+        for tag in self._reaction_q.keys():
+            if tag <= self._current_tag:
+                tags_to_remove.append(tag)
+        for tag in tags_to_remove:
+            del self._reaction_q[tag]
 
     def _schedule_timers_once(self) -> None:
-        with self._reaction_q_cv:
-            for timer in self._timers:
-                timer.schedule_start()
+        for timer in self._timers:
+            timer.schedule_start()
 
     def _get_triggered_reactions(self, action : TriggerAction):
         triggered_reactions : List[Reaction] = []
         for reaction in self._reactions:
             if action in reaction.triggers:
                 triggered_reactions.append(reaction)
-        return triggered_reactions
+        return triggered_reactions  
 
+    def wait_for(self, predicate):
+        self._reaction_q_cv.wait_for(predicate)
 
     def run(self):
-        self._schedule_timers_once()
+        with self._reaction_q_cv:
+            self._schedule_timers_once()
+        stop_running : bool = False
         while True:
             actions = []
             with self._reaction_q_cv:
-                self._release_current_tag()
                 if not self._reaction_q:
-                    return
-                next : ActionListEntry = self._reaction_q.get_next()
-                self._current_tag = next.tag
-                actions = next.actions
+                    self.wait_for(lambda: bool(self._reaction_q))
+
+                next_tag : Tag = self._reaction_q.next_tag()
+                refetch_next_tag : bool = False
+                for input in self._inputs:
+                    result : bool = input.acquire_tag(next_tag, lambda: self._reaction_q.next_tag() != next_tag)
+                    if not result or self._reaction_q.next_tag() != next_tag: # acquire_tag failed or actionlist modified
+                        refetch_next_tag = True
+                        break
+                if refetch_next_tag:
+                    continue
+
+                self._current_tag = next_tag
+                actions = self._reaction_q.pop_tag(self._current_tag)
+
                 triggered_actions = []
                 for action in actions:
                     if isinstance(action, TriggerAction):
                         triggered_actions += self._get_triggered_reactions(action)
                 actions += triggered_actions
+                self._release_current_tag()
+                if self._current_tag == self._stop_tag:
+                    stop_running = True
             for action in actions:
                 action.exec(self._current_tag)
+            if stop_running:
+                return
 
 
 
